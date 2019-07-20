ac00eb17735b9481282fb88383697409c1917c72
David Little <littledavid175@gmail.com>
2019-07-20 20:49:24 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -19,7 +19,14 @@ end
 def monopoly_with_third_tier
 	monopoly = {
 	  :railroads =>
+<<<<<<< HEAD
 	  {:pieces => 4, :names => { :reading_railroad => {}, :pennsylvania_railroad => {}, :b_and_o_railroad => {}, :shortline =>{}}, :rent_in_dollars =>{ :one_piece_owned => 25, :two_pieces_owned => 50, :three_pieces_owned => 100, :four_pieces_owned => 200}
+=======
+	  {:pieces => 4, :names => {}
+	  :rent_in_dollars =>{
+	    :one_piece_owned => 25, :two_pieces_owned => 50, :three_pieces_owned => 100, :four_pieces_owned => 200
+	  }
+>>>>>>> 1d7981eb9e70247cabfb207d2db3a66829bd0d9d
 	  }
 	}
 
